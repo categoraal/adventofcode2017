d85d267836bca2a570376904dd0e4b9380fe7787
categoraal <76538493+categoraal@users.noreply.github.com>
2024-02-11 19:13:42 +0100

    day 19

---
+++ day19.py
@@ -0,0 +1,36 @@
+from collections import deque
+data = open('in19').read().split('\n')[:-2]
+
+queue = [(0,data[0].index('|'))]
+res = ''
+d = (1,0)
+cnt = 0
+for r,c in queue:
+    dr,dc = d
+    seg = data[r][c]
+    if seg == ' ':
+        break
+
+    if seg not in '-|+':
+        res += seg
+    
+    if seg == '+':
+        for nd in ((-dc,dr),(dc,-dr)):
+            dr,dc = nd
+            nr = r+dr;nc = c+dc
+            # print(nr,nc)
+            if 0 <= nr < len(data) and 0<=nc<len(data[0]):
+                if data[nr][nc] != ' ':
+                    d = nd
+
+    dr,dc = d
+    nr = r+dr;nc = c+dc
+    if 0<=nr<len(data) and 0<=nc<len(data[0]):
+        queue.append((nr,nc))
+        cnt += 1
+
+
+print(res)
+print(cnt)
+
+            
